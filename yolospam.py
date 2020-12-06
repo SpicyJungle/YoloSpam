d9df9aaed6964d4bcdd502b22abd5aebc6559d44
SpicyJungle <45978658+SpicyJungle@users.noreply.github.com>
2020-12-06 19:30:49 +0100

    Update yolospam.py

--- yolospam.py
+++ yolospam.py
@@ -3,19 +3,17 @@ import time
 import random
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\eiken\Downloads\chromedriver_win32\chromedriver.exe')
-driver.get('')
-ans = ['Heil stalin', 'Stalin <3', 'Long Live Stalin', 'StalinLivesMatter', 'Long live stalin!', "stalin e babe",
-       'Stalin er mitt forbilde.'
-       ]
+driver = webdriver.Chrome(executable_path=r'') #Put path to webdriver. (You will need to change webdriver.Chrome to webdriver.{yourdriver} if you don't use the chrome driver.
+driver.get('')#Yolo link goes here
+ans = []#Write responses here for random responses
 while 1:
     try:
         sendButtone = driver.find_element_by_id('send-button')
         textBox = driver.find_element_by_id('text')
-        time.sleep(0.5)
-        textBox.send_keys(random.choice(ans))
+        time.sleep(0.1)
+        textBox.send_keys()#Text here, or random.choice(ans)
         sendButtone.click()
-        driver.refresh()
-    except StaleElementReferenceException:
+        driver.refresh()#Refresh to enable text box again
+    except StaleElementReferenceException: #Incase page doesnt load fast enough
         print("stale")
         continue
